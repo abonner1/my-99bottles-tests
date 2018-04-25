1522490b2bef47bc032f043a05f9fa254c3b3b63
Andrew Bonner <andrewbonner92@gmail.com>
2018-04-25 10:08:31 -0400

    Pass 0 bottles test.

--- lib/bottle.rb
+++ lib/bottle.rb
@@ -7,6 +7,9 @@ class Bottle
     when 1
       "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" +
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
       "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
