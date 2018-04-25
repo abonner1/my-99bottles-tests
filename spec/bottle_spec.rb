bf1361b41d8499b5bcfdef1d8720ec380f1e42de
Andrew Bonner <andrewbonner92@gmail.com>
2018-04-25 09:53:53 -0400

    Pass verse 2 test.

--- spec/bottle_spec.rb
+++ spec/bottle_spec.rb
@@ -1,21 +1,26 @@
 require "bottle.rb"
 
 describe Bottle do
-  it "returns the first verse" do
-    verse = "99 bottles of beer on the wall, " +
-            "99 bottles of beer.\n" +
-            "Take one down and pass it around, " +
-            "98 bottles of beer on the wall.\n"
+  let(:bottle) { Bottle.new }
 
-    expect(Bottle.new.verse(99)).to eq(verse)
+  it "returns the first verse" do
+    verse = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+            "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+
+    expect(bottle.verse(99)).to eq(verse)
   end
 
   it "returns another verse" do
-    another_verse = "3 bottles of beer on the wall, " +
-                    "3 bottles of beer.\n" +
-                    "Take one down and pass it around, " +
-                    "2 bottles of beer on the wall.\n"
+    another_verse = "3 bottles of beer on the wall, 3 bottles of beer.\n" +
+                    "Take one down and pass it around, 2 bottles of beer on the wall.\n"
 
-    expect(Bottle.new.verse(3)).to eq(another_verse)
+    expect(bottle.verse(3)).to eq(another_verse)
+  end
+
+  it "returns verse 2 making bottles singular" do
+    second_verse = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+                   "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+
+    expect(bottle.verse(2)).to eq(second_verse)
   end
 end
