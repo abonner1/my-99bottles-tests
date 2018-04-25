4253341828585d6e439ba8832b25611e7e678e4c
Andrew Bonner <andrewbonner92@gmail.com>
2018-04-25 10:00:59 -0400

    Pass 1 bottle test and refactor.

--- lib/bottle.rb
+++ lib/bottle.rb
@@ -1,8 +1,12 @@
 class Bottle
   def verse(number)
-    if number == 2
+    case number
+    when 2
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
       "Take one down and pass it around, #{number-1} bottle of beer on the wall.\n"
+    when 1
+      "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" +
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
       "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
--- spec/bottle_spec.rb
+++ spec/bottle_spec.rb
@@ -3,31 +3,33 @@ require "bottle.rb"
 describe Bottle do
   let(:bottle) { Bottle.new }
 
-  it "returns a verse" do
-    verse = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-            "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+  context "#verse" do
+    it "returns a verse" do
+      verse = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+              "Take one down and pass it around, 98 bottles of beer on the wall.\n"
 
-    expect(bottle.verse(99)).to eq(verse)
-  end
+      expect(bottle.verse(99)).to eq(verse)
+    end
 
-  it "returns another verse" do
-    another_verse = "3 bottles of beer on the wall, 3 bottles of beer.\n" +
-                    "Take one down and pass it around, 2 bottles of beer on the wall.\n"
+    it "returns another verse" do
+      another_verse = "3 bottles of beer on the wall, 3 bottles of beer.\n" +
+                      "Take one down and pass it around, 2 bottles of beer on the wall.\n"
 
-    expect(bottle.verse(3)).to eq(another_verse)
-  end
+      expect(bottle.verse(3)).to eq(another_verse)
+    end
 
-  it "returns the verse for 2 bottles" do
-    second_verse = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-                   "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    it "returns the verse for 2 bottles" do
+      second_verse = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+                     "Take one down and pass it around, 1 bottle of beer on the wall.\n"
 
-    expect(bottle.verse(2)).to eq(second_verse)
-  end
+      expect(bottle.verse(2)).to eq(second_verse)
+    end
 
-  it "returns the verse for 1 bottle" do
-    first_verse = "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-                  "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    it "returns the verse for 1 bottle" do
+      first_verse = "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                    "Take it down and pass it around, no more bottles of beer on the wall.\n"
 
-    expect(bottle.verse(1)).to eq(first_verse)
+      expect(bottle.verse(1)).to eq(first_verse)
+    end
   end
 end
