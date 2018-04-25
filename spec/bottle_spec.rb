7a74a328a69b9f1b5a34a12f47d2c5e53e14dca4
Andrew Bonner <andrewbonner92@gmail.com>
2018-04-25 10:22:51 -0400

    Refactor.

--- spec/bottle_spec.rb
+++ spec/bottle_spec.rb
@@ -2,6 +2,18 @@ require "bottle.rb"
 
 describe Bottle do
   let(:bottle) { Bottle.new }
+  let(:two_bottle_verse) {
+    "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+    "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+  }
+  let(:one_bottle_verse) {
+    "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+    "Take it down and pass it around, no more bottles of beer on the wall.\n"
+  }
+  let(:no_bottle_verse) {
+    "No more bottles of beer on the wall, no more bottles of beer.\n" +
+    "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+  }
 
   context "#verse" do
     it "returns a verse" do
@@ -19,24 +31,15 @@ describe Bottle do
     end
 
     it "returns the verse for 2 bottles" do
-      two_bottle_verse = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-                     "Take one down and pass it around, 1 bottle of beer on the wall.\n"
-
-      expect(bottle.verse(2)).to eq(second_verse)
+      expect(bottle.verse(2)).to eq(two_bottle_verse)
     end
 
     it "returns the verse for 1 bottle" do
-      one_bottle_verse = "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-                    "Take it down and pass it around, no more bottles of beer on the wall.\n"
-
-      expect(bottle.verse(1)).to eq(first_verse)
+      expect(bottle.verse(1)).to eq(one_bottle_verse)
     end
 
     it "returns the verse for 0 bottles" do
-      no_bottle_verse = "No more bottles of beer on the wall, no more bottles of beer.\n" +
-                    "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-
-      expect(bottle.verse(0)).to eq(final_verse)
+      expect(bottle.verse(0)).to eq(no_bottle_verse)
     end
   end
 
@@ -48,6 +51,12 @@ describe Bottle do
               "Take one down and pass it around, 97 bottles of beer on the wall.\n"
 
       expect(bottle.verses(99, 98)).to eq(verses)
+    end
+
+    it "returns the last three verses" do
+      last_three_verses = two_bottle_verse + one_bottle_verse + no_bottle_verse
+
+      expect(bottle.verses(2, 0)).to eq(last_three_verses)
     end
   end
 end
