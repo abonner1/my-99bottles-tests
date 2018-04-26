c6a52410bca87a504bfdd4f71be6ccb5b379c1ac
Andrew Bonner <andrewbonner92@gmail.com>
2018-04-26 10:15:33 -0400

    Refactor bottle/s into #container method.

--- lib/bottle.rb
+++ lib/bottle.rb
@@ -9,9 +9,6 @@ class Bottle
 
   def verse(number)
     case number
-    when 2
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
-      "Take one down and pass it around, #{number-1} bottle of beer on the wall.\n"
     when 1
       "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" +
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
@@ -20,7 +17,15 @@ class Bottle
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
-      "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+      "Take one down and pass it around, #{number-1} #{container(number-1)} of beer on the wall.\n"
+    end
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
     end
   end
 end
